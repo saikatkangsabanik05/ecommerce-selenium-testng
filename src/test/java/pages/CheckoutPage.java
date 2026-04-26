e185e9bd89ead33dbfb438a06cb96da60941e9ff
Saikat <saikatkangsabanik1999@gmail.com>
2026-04-26 14:37:11 +0530

    Fix: add missing JavascriptExecutor import in CheckoutPage

--- src/test/java/pages/CheckoutPage.java
+++ src/test/java/pages/CheckoutPage.java
@@ -1,6 +1,8 @@
 package pages;
 
 import base.BasePage;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
